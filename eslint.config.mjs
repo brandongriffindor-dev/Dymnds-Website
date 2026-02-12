c917c22951d10cfa1a09617be5f9072d95650e53
brandongriffindor-dev <brandongriffindor@gmail.com>
2026-02-11 22:21:43 -0700

    Update ESLint configuration to modify TypeScript rules
    Disable specific TypeScript ESLint rules for flexibility.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,17 @@ const eslintConfig = [
   },
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@next/next/no-assign-module-variable": "off",
+      "react-compiler/react-compiler": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
